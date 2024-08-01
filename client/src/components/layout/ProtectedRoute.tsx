4043f0a46128521f0ff9b4e3ceb05769be440b04
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-08-01 20:08:28 +0600

    console deleted

--- client/src/components/layout/ProtectedRoute.tsx
+++ client/src/components/layout/ProtectedRoute.tsx
@@ -5,13 +5,11 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const token = useAppSelector(useCurrentToken);
-  console.log("🚀 ~ ProtectedRoute ~ token:", token);
+
   if (!token) {
     return <Navigate to={"/login"} replace={true} />;
-  } else {
-    <Navigate to={"/"} replace={true} />;
-    return children;
   }
+  return children;
 };
 
 export default ProtectedRoute;
